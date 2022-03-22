8d4b9356e5dc0370737e328598ac88765744c189
addehd <hej@adolfo.se>
2022-03-22 23:11:59 +0100

    use % for better positioning of zoomed gallery images

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -4,7 +4,7 @@ import Estorme from "../images/gallery/estorme.jpg"
 
 export default function Slideshow(imgData) {
   //https://stackoverflow.com/questions/65840192/loop-through-an-array-and-show-single-item-at-a-time-with-dynamic-time-duration
-  const items = [{ name: "bottom left" }, { name: "bottom right" }, { name: "top left" }];
+  const items = [{ name: " 0% 0%" }, { name: " 5% 70%" }, { name: " 50% 5%" }, { name: " 30% 5%" }];
   const [mediaItem, setMediaItem] = useState(items[0]);
   const [index, setIndex] = useState(0);
 
@@ -25,8 +25,12 @@ export default function Slideshow(imgData) {
       {console.log(mediaItem.name)}
       {/* <h1 style={{color: mediaItem.name, fontSize: "1rem"}}>Test</h1> */}
       {/* <StaticImage className="gallery-image" imgStyle={{transformOrigin: mediaItem.name }} src="../images/gallery/estorme.jpg" alt="art" /> */}
+      {/* style={{transformOrigin: mediaItem.name }} */}
      <div className='gallery-image-wrapper'>
-        <img className="gallery-image" style={{transformOrigin: mediaItem.name }} src={Estorme} alt="art" />
+        <img className="gallery-image"  style={{transformOrigin: mediaItem.name }} src={Estorme} alt="art" />
+      </div>
+      <div style={{ height: "100vh", display: "flex", alignItems: "baseline" }} >
+        <img style={{width: "50%" }} src={Estorme} alt="art" />
       </div>
     </>
   )
