f1e6d844a467b8559fd4cb1cb6ba73b9476c5dec
addehd <hej@adolfo.se>
2022-03-23 23:30:10 +0100

    fixed side header and solo img styles

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -4,6 +4,7 @@ import Estorme from "../images/gallery/estorme.jpg"
 
 export default function Slideshow(imgData) {
   //https://stackoverflow.com/questions/65840192/loop-through-an-array-and-show-single-item-at-a-time-with-dynamic-time-duration
+  //https://www.gatsbyjs.com/plugins/gatsby-image/
   const items = [{ name: " 0% 0%" }, { name: " 5% 70%" }, { name: " 50% 5%" }, { name: " 30% 5%" }];
   const [mediaItem, setMediaItem] = useState(items[0]);
   const [index, setIndex] = useState(0);
@@ -22,14 +23,12 @@ export default function Slideshow(imgData) {
 
   return (
     <>
-      {console.log(mediaItem.name)}
-      {/* <h1 style={{color: mediaItem.name, fontSize: "1rem"}}>Test</h1> */}
       {/* <StaticImage className="gallery-image" imgStyle={{transformOrigin: mediaItem.name }} src="../images/gallery/estorme.jpg" alt="art" /> */}
-      {/* style={{transformOrigin: mediaItem.name }} */}
-     <div className='gallery-image-wrapper'>
+      <div className='gallery-image-wrapper'>
         <img className="gallery-image"  style={{transformOrigin: mediaItem.name }} src={Estorme} alt="art" />
       </div>
-      <div style={{ height: "100vh", display: "flex", alignItems: "baseline" }} >
+      { console.log(mediaItem.name) }
+      <div className="solo-image" >
         <img style={{width: "50%" }} src={Estorme} alt="art" />
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,15 @@ import * as React from "react"
 import logo from "../media/maria-logo.svg"
 import SlideShow from "../components/slideshow"
 
-
-const side = {
-  color: "blue",
-  position: "absolute",
-  height: "100vh",
-  position: "fixed",
-  width: "1rem",
-  top: 0,
-  padding: "1rem",
-  left: "0"
-}
-
-//data
 const IndexPage = () => {
   return (
     <main>
-      <header>
-        <img style={side} src={logo}/>
+      <header className="logo-wrap">
+        <img className="logo-side" src={logo}/>
       </header>
       <title>Home Page</title>
       <SlideShow imgData="imgDataTxt" />
     </main>
-  )
-}
+  ) }
 
 export default IndexPage
